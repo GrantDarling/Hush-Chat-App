091751655fa3681e7e50cc68807e370eee304c9c
Grant Darling <grantcarterdarling@gmail.com>
2021-02-26 18:40:23 -0500

    Created Client Connection - Create console log that displays between client consoles - Created javascript generated divs that display between consoles

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require('express')();
-const http = require('http');
-const server = http.createServer(app);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:5000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     transports: ['websocket', 'polling'],
     credentials: true
@@ -11,33 +11,20 @@ const io = require('socket.io')(server, {
   allowEIO3: true
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/index', (req, res) => {
-  res.sendFile(__dirname + '/');
-});
-
-// add middleware
-//app.use(express.static('client/src'));
-
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const { id } = socket.client;
+  console.log(`${id} connected`);
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    console.log(`${id} disconnected`);
   });
 
   socket.on('chat message', (msg) => {
+    console.log(`Message: ${msg}`);
     io.emit('chat message', msg);
   });
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log('listening on localhost:' + PORT);
-});
+server.listen(PORT, () => console.log(`Listen on localhost:${PORT}...`));
