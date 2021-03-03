e0eb44a483d0cb7b20ca1ef8c31c6ef7a4321863
Grant Darling <grantcarterdarling@gmail.com>
2021-03-03 13:43:26 -0500

    first commit - Removed test code from server.js - Removed test code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,81 +6,10 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:5000');
 
 let App = () => {
-  const [message, setMessage] = useState({
-    text: '',
-    sender: ''
-  });
-
-  useEffect(() => {
-    socket.on('chat message', function (msg, id) {
-      const texts = document.getElementById('texts');
-      const item = document.createElement('p');
-
-      console.log(msg);
-      item.textContent = `${id} says: ${msg}`; //User ${id}:
-      texts.appendChild(item);
-
-      // Create room
-    });
-    let counter = 0;
-    socket.on('connection', (id) => {
-      counter++;
-      let room;
-      console.log(id + ' ' + room + ' ' + counter);
-
-      socket.emit('create', room); // Randomly generated and on click
-
-      if (counter === 'room2') {
-        room = 'room1';
-      }
-
-      setMessage({ sender: id });
-      playVideoFromCamera();
-    });
-
-    // Video recording
-    const openMediaDevices = async (constraints) => {
-      return await navigator.mediaDevices.getUserMedia(constraints);
-    };
-
-    try {
-      const stream = openMediaDevices({ video: true, audio: true });
-      console.log('Got MediaStream:', stream);
-    } catch (error) {
-      console.error('Error accessing media devices.', error);
-    }
-
-    async function playVideoFromCamera() {
-      try {
-        const constraints = { video: true, audio: false }; // Make audio true
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        const videoElement = document.querySelector('video#localVideo');
-        videoElement.srcObject = stream;
-      } catch (error) {
-        console.error('Error opening video camera.', error);
-      }
-    }
-
-    playVideoFromCamera();
-  }, []);
-
-  const onChange = (e) => setMessage({ text: e.target.value });
-
-  const onSubmit = () => {
-    socket.emit('chat message', message.text);
-
-    setMessage({ text: '' });
-  };
-  // <video autoplay playsinline controls='false' />;
   return (
     <div className='container'>
-      <h1>F**k this app.</h1>
-      <div id='texts'>
-        <input onChange={(e) => onChange(e)} value={message.text} />
-        <button onClick={onSubmit}>Send</button>
-        <h1>User {message.sender}</h1>
-        <video id='localVideo' autoPlay playsInline controls={false} />
-      </div>
+      <h1>Hush app.</h1>
+      <div id='texts'></div>
     </div>
   );
 };
--- server.js
+++ server.js
@@ -1,37 +1,6 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    transports: ['websocket', 'polling'],
-    credentials: true
-  },
-  allowEIO3: true
-});
-
-io.on('connection', (socket) => {
-  const { id } = socket.client;
-
-  io.emit('connection', id);
-
-  // Create room
-  socket.on('create', function (room) {
-    socket.join(room);
-    console.log(`${id} connected to ${room}`);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`${id} disconnected`);
-  });
-
-  socket.on('chat message', (message) => {
-    console.log(`Message: ${message}, ID: ${socket.client.id}`);
-    const id = socket.client.id;
-    io.emit('chat message', message, id);
-  });
-});
 
 app.get('/', (res, req) => {
   req.send('Hush Chat/Video App Server');
