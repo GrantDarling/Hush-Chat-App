a61dd227d6385cf4fadd9ddeef754384a268d496
Grant Darling <grantcarterdarling@gmail.com>
2021-03-11 23:29:11 -0500

    Minor tweaks to lobby page design

--- client/src/components/layout/Lobby.js
+++ client/src/components/layout/Lobby.js
@@ -12,9 +12,9 @@ const Lobby = () => {
       <div className='chatrooms'>
         <div className='chatroom'>
           <div className='details-container'>
-            <h3><small>Chatroom: </small>Cool Kids</h3>
+            <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
-            <h3><small>Capacity: </small>1/2</h3>
+            <h4>Capacity: 1/2</h4>
           </div>
           <a href='/lobby' className='join'>JOIN</a>
         </div>
@@ -22,9 +22,9 @@ const Lobby = () => {
       {/* Chatroom locked */}
         <div className='chatroom--locked'>
           <div className='details-container'>
-            <h3><small>Chatroom: </small>Cool Kids</h3>
+            <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
-            <h3><small>Capacity: </small>1/2</h3>
+            <h4>Capacity: 2/2</h4>
           </div>
           <div className='join'>FULL</div>
         </div>
@@ -32,9 +32,9 @@ const Lobby = () => {
       {/* Sample Third */}
         <div className='chatroom'>
           <div className='details-container'>
-            <h3><small>Chatroom: </small>Cool Kids</h3>
+            <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
-            <h3><small>Capacity: </small>1/2</h3>
+            <h4>Capacity: 1/2</h4>
           </div>
           <div className='join'>JOIN</div>
         </div> 
