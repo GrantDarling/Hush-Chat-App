3e2e2374799db01c48f0882144f8cff0ff4d6b6b
Grant Darling <grantcarterdarling@gmail.com>
2021-03-16 23:02:38 -0400

    Basic styling for room component

--- client/src/components/layout/Room.js
+++ client/src/components/layout/Room.js
@@ -1,29 +1,68 @@
 import React from 'react';
-import Modal from './Modal';
-import CreateRoom from './ModalCreateRoom';
+// import Modal from './Modal';
+// import CreateRoom from './ModalCreateRoom';
+import HostPlaceholder from "../../images/user-placeholder1.png";
+import GuestPlaceholder from "../../images/user-placeholder2.png";
 
 const Room = () => {
     // Fix the CSS class names 
   return (
-    <section className='room-container'> {// Make this class *Room 
-    }
-        <div className='chatbox'>
-            <div className='chatbox__text-view'>
-                <ul id="chatbox__text-view">
-                    <li className='chatbox__text-message'>This is a sample message</li>
-                </ul>
+    <section className='Room'>
+        <h3 className='Room-name'>Chatroom: The Cool Kids</h3>
+        <div className='chatbox'> 
+            <div className='chat'>
+                <div className='messages'>
+                    <ul className="message-emit">
+                        <li>'The Cool Kids' Group Created...</li>
+                        <li>@Grant69 has entered the chat.</li>
+                        <li>@LukeSkywalker has entered the chat.</li>
+                    </ul>
+                    <div className='message-host'>
+                        <h3>@grantd16</h3>
+                        <p>Hey man!</p>
+                    </div>
+                    <div className='message-guest'>
+                        <h3>@lukeskywalker</h3>
+                        <p>Hey man!</p>
+                    </div>
+                    <div className='message-guest'>
+                        <h3>@lukeskywalker</h3>
+                        <p>This is an example of a super 
+                            Long message that I hope isn’t 
+                            Too long but it needed to be 
+                            done !
+                        </p>
+                    </div>
+                    <div className='message-host'>
+                        <h3>@lukeskywalker</h3>
+                        <p>This is an example of a super 
+                            Long message that I hope isn’t 
+                            Too long but it needed to be 
+                            done !
+                        </p>
+                    </div>
+                </div>
+                <form className='input-controller'>
+                    <input type="text" name='message' />
+                    <button type='submit'>SEND</button>
+                </form>
             </div>
-            <form className='chatbox__input'>
-                <input type="text" name='message' />
-                <button type='submit'>Send</button>
-            </form>
-        </div>
-        <div className='videos'>
-            <video className='room__your-video' />
-            <video className='room__guest-video' />
-        </div>
+            <div className='videos'>
+                <div className='host'>
+                    <h4 className='username' >@Grant69</h4>
+                    <img src={HostPlaceholder}  alt="Host Placeholder"/>
+                </div>
+                <div className='guest'>
+                    <h4 className='username' >@LukeSkywalker</h4>
+                    <img src={GuestPlaceholder} alt="Guest Placeholder"/>
+                </div>
 
-        <Modal><CreateRoom /></Modal>
+                {/* <video className='host' />
+                <video className='guest' /> */}
+            </div>
+
+            {/* <Modal><CreateRoom /></Modal> */}
+        </div>
     </section>
   );
 };
