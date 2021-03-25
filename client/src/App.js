57d54fa29d7914157a8ad27ac2338cf1f996649a
Grant Darling <grantcarterdarling@gmail.com>
2021-03-24 22:16:11 -0400

    connected socket io and displayed connected msg

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 // Import Component
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Routes from './components/routes/Routes';
 import './App.scss';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:5000');
 
 let App = () => {
+
+  useEffect(() => {
+    socket.on("connection", (id) => {
+      console.log(`${id} connected...`); // world
+    });
+  })
   return (
     <Router>
       <Navbar />
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const { emit } = require('process');
 const app = express();
 const server = require('http').Server(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+io.on("connection", (socket) => {
+  const {id} = socket;
+
+  socket.emit("connection", id);
+
+  console.log(`${id} connected...`)
+});
 
 app.get('/', (res, req) => {
   req.send('Hush App Server...');
