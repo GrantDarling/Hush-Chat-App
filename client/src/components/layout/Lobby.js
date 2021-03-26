2700279034d7e14ab2f31027eae1d68b9ee28d90
Grant Darling <grantcarterdarling@gmail.com>
2021-03-26 18:06:27 -0400

    Cleaned up lobby.js codebase

--- client/src/components/layout/Lobby.js
+++ client/src/components/layout/Lobby.js
@@ -8,15 +8,16 @@ const socket = io.connect('http://localhost:5000');
 
 
 const Lobby = () => {
-  const [isOpen, toggleModal] = ModalSwitch();
+  const [isOpen, toggleModal] = ModalSwitch(); // !!! potentially unused
   const [lobby, setLobby] = useState([]);
 
   useEffect(() => {
+
+    // Get list of available rooms
     socket.on('get rooms', (activeRooms) => {
       console.log(activeRooms);
       setLobby(activeRooms)
     });
-      
     socket.emit('get rooms');
 
   }, []);
@@ -24,18 +25,36 @@ const Lobby = () => {
   return (
     <section className='Lobby'>
       <div className='chatrooms'>
-        {console.log(lobby.length)}
-      {lobby.length > 0 ? lobby.map((room) => (
-          <div className='chatroom'>
-            <div className='details-container'>
-              <h3><small>Room Name: </small>{room}</h3>
-              <h3><small>Host: </small>Kyle88</h3>
-              <h4>Capacity: 1/2</h4>
+
+        {lobby.length > 0 ? lobby.map((room) => 
+          (
+            <div className='chatroom'>
+              <div className='details-container'>
+                <h3><small>Room Name: </small>{room}</h3>
+                <h3><small>Host: </small>Kyle88</h3>
+                <h4>Capacity: 1/2</h4>
+              </div>
+              <NavLink exact to='/room' className='join' onClick={toggleModal}>JOIN</NavLink>
             </div>
-            <NavLink exact to='/room' className='join' onClick={toggleModal}>JOIN</NavLink>
-          </div>
-      )) : <p>nada</p>
-    };
+          )) : (
+            <div class="chatrooms--none">
+              <h1>Currently no chatrooms available.</h1>
+            </div> 
+          )
+        }
+
+        <Modal isOpen={isOpen} toggleModal={toggleModal}> {/* !!! potentially unused */}
+          <JoinRoom />
+        </Modal>
+
+      </div>
+    </section>
+    )
+};
+
+export default Lobby;
+
+
 
        {/* No Chatrooms */}
       {/* <div class="chatrooms--none">
@@ -43,27 +62,27 @@ const Lobby = () => {
       </div> */}
 
       {/* Chatroom unlocked */}
-        <div className='chatroom'>
+        {/* <div className='chatroom'>
           <div className='details-container'>
             <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
             <h4>Capacity: 1/2</h4>
           </div>
           <NavLink exact to='/lobby' className='join' onClick={toggleModal}>JOIN</NavLink>
-        </div>
+        </div> */}
 
       {/* Chatroom locked */}
-        <div className='chatroom--locked'>
+        {/* <div className='chatroom--locked'>
           <div className='details-container'>
             <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
             <h4>Capacity: 2/2</h4>
           </div>
           <div className='join'>FULL</div>
-        </div>
+        </div> */}
 
       {/* Sample Third */}
-        <div className='chatroom'>
+        {/* <div className='chatroom'>
           <div className='details-container'>
             <h3><small>Room Name: </small>Cool Kids</h3>
             <h3><small>Host: </small>Kyle88</h3>
@@ -71,13 +90,5 @@ const Lobby = () => {
           </div>
           <div className='join'>JOIN</div>
         </div> 
-      </div>
-
-      <Modal isOpen={isOpen} toggleModal={toggleModal}>
-        <JoinRoom />
-      </Modal>
-    </section>
-  );
-};
-
-export default Lobby;
+      </div> 
+  ); */}
