155000a718115b3d90e362fa1f03bdc2e5c0c9d5
Grant Darling <grantcarterdarling@gmail.com>
2021-04-07 22:42:04 -0400

    general file removal and restructure

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const { onSocket } = require('./socketAPI');
+const { connectSocket } = require('./socketAPI');
 const app = express();
-const socketAPI = require('./socketAPI');
 const server = require('http').Server(app);
-const io = require("socket.io")(server, { cors: 
-  { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
+const io = require("socket.io")(server, { 
+  cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
 });
 
 // Connect SocketIO
-socketAPI.connectSocket(io);
+connectSocket(io);
 
 // Start server
 app.get('/', (res, req) => req.send('Hush App Server...'));
