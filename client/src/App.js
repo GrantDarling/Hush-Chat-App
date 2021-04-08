db321aa269441f466517926d84b91832952a2a98
Grant Darling <grantcarterdarling@gmail.com>
2021-04-07 22:23:21 -0400

    refactored app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Routes from './components/routes/Routes';
-
 import io from 'socket.io-client';
+
 const socket = io.connect('http://localhost:5000');
 
 const App = () => {
