1f4ada3131fc2c1496712df09659e0fe9abc967c
Grant Darling <grantcarterdarling@gmail.com>
2021-04-09 19:59:11 -0400

    created onWebRTC & onWebRTC2 functions

--- client/src/components/layout/Room.js
+++ client/src/components/layout/Room.js
@@ -53,60 +53,7 @@ const Room = ({ state, socket }) => {
     useEffect(() => {
         if(isCreated) {
 
-        const peerConnections = {};
-        const config = {
-            iceServers: [{ "urls": "stun:stun.l.google.com:19302" }]
-        };   
-
-        // Answer the call and set remote description
-        socket.on("answer", (id, description) => {
-            peerConnections[id].setRemoteDescription(description);
-            console.log(`Peer connection on answer: ${peerConnections[id]} \n description param: ${description}`)
-        });
-        console.log('host peerConnections ' + peerConnections)
-
-        // On watcher...
-        socket.on("watcher", id => {
-        const peerConnection = new RTCPeerConnection(config); // Create new connection
-        peerConnections[id] = peerConnection; // Assign specific id in object to peerConnection
-
-        let stream = videoElement.current.srcObject; // Assign stream from video element
-        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream)); // Assign tracks 
-
-        peerConnection.onicecandidate = event => { // Event listener for ice candidate
-            if (event.candidate) {
-            socket.emit("candidate", id, event.candidate); // Emit candidate when event fires
-            }
-        };
-
-            console.log('let\'s connect baby!!')
-            // Create offer, set local descrp. then emit offer
-            peerConnection 
-                .createOffer()
-                .then(sdp => peerConnection.setLocalDescription(sdp))
-                .then(() => {
-                socket.emit("offer", id, peerConnection.localDescription);
-                });
-
-        })
-
-
-
-        socket.on("candidate", (id, candidate) => {
-            peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
-        });
-
-        // socket.on("disconnectPeer", id => {
-        //     peerConnections[id].close();
-        //     delete peerConnections[id];
-        // });
-
-        // window.onunload = window.onbeforeunload = () => {
-        // socket.close();
-        // };
-
-
-
+    onWebRTC(socket, videoElement);
     displayUserMedia(socket, videoElement, "broadcaster");
 }
     }, [socket, videoElement, isCreated]);
@@ -218,63 +165,7 @@ const Room = ({ state, socket }) => {
         video.current.muted = false;
         }
 
-
-
-
-        // // Create my own connection! *****
-
-        const peerConnections = {};
-
-        // Answer the call and set remote description
-        socket.on("answer2", (id, description) => {
-            peerConnections[id].setRemoteDescription(description);
-            console.log(`Peer connection on answer: ${peerConnections[id]} \n description param: ${description}`)
-        });
-        console.log('host peerConnections ' + peerConnections)
-
-        // On watcher...
-        socket.on("watcher2", id => {
-            console.log("watcher started!")
-        const peerConnection = new RTCPeerConnection(config); // Create new connection
-        peerConnections[id] = peerConnection; // Assign specific id in object to peerConnection
-
-        let stream = videoElement2.current.srcObject; // Assign stream from video element
-        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream)); // Assign tracks 
-
-        peerConnection.onicecandidate = event => { // Event listener for ice candidate
-            if (event.candidate) {
-            socket.emit("candidate2", id, event.candidate); // Emit candidate when event fires
-            }
-        };
-
-            console.log('let\'s connect baby!!')
-            // Create offer, set local descrp. then emit offer
-            peerConnection 
-                .createOffer()
-                .then(sdp => peerConnection.setLocalDescription(sdp))
-                .then(() => {
-                socket.emit("offer2", id, peerConnection.localDescription);
-                });
-
-        })
-
-
-
-        socket.on("candidate2", (id, candidate) => {
-            peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
-        });
-
-        // // socket.on("disconnectPeer", id => {
-        // //     peerConnections[id].close();
-        // //     delete peerConnections[id];
-        // // });
-
-        // window.onunload = window.onbeforeunload = () => {
-        // socket.close();
-        // };
-
-
-
+        onWebRTC2(socket, videoElement2);
         displayUserMedia(socket, videoElement2, "broadcaster2");
 
         }
