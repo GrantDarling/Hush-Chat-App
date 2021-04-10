f4661e6a8e8e9e9c6df311076ac48d383082594f
Grant Darling <grantcarterdarling@gmail.com>
2021-04-10 19:38:21 -0400

    ff

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ connectSocket(io);
 // Start server
 app.get('/', (res, req) => req.send('Hush App Server...'));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')))
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/client/dist', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Listen on localhost:${PORT}...`));
