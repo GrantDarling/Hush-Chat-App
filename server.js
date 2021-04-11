08a358a176c28e121c95baddc94646948ec4dbd1
Grant Darling <grantcarterdarling@gmail.com>
2021-04-10 20:06:15 -0400

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const { connectSocket } = require('./socketAPI');
 const app = express();
+const path = require('path');
 const server = require('http').Server(app);
 const io = require("socket.io")(server, { 
   cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
 });
 
+
 // Connect SocketIO
 connectSocket(io);
 
-// Start server
-app.get('/', (res, req) => req.send('Hush App Server...'));
-
+// Serve static assets in production 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')))
-  app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/client/dist', 'index.html'))
-  })
+  // Set static folder 
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
