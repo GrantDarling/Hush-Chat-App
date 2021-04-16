6a4446960f93c3ef0673be5448ab3d83ff7324f9
Grant Darling <grantcarterdarling@gmail.com>
2021-04-15 20:53:11 -0400

    cors fix #3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors')
 const server = require('http').Server(app);
 const io = require("socket.io")(server, { 
-  cors: { origin: 'https://hush-chat-app.herokuapp.com/socket.io/?EIO=4&transport=polling&t=NZO8bci', methods: ['GET', 'POST'] }
+  cors: { origin: 'https://hush-chat-app.herokuapp.com/*', methods: ['GET', 'POST'] }
 });
 
 // Connect SocketIO
