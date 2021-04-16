f11093a0a6e1e556ffe4f9046b7604656cc6a92c
Grant Darling <grantcarterdarling@gmail.com>
2021-04-15 20:58:26 -0400

    Force https cors fix

--- server.js
+++ server.js
@@ -5,17 +5,27 @@ const path = require('path');
 const cors = require('cors')
 const server = require('http').Server(app);
 const io = require("socket.io")(server, { 
-  cors: { origin: 'https://hush-chat-app.herokuapp.com/*', methods: ['GET', 'POST'] }
+  cors: { origin: 'https://hush-chat-app.herokuapp.com/', methods: ['GET', 'POST'] }
 });
 
 // Connect SocketIO
 app.use(cors());
 connectSocket(io);
 
+// Require https
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 // Serve static assets in production 
 if(process.env.NODE_ENV === 'production') {
-  // Set static folder 
-  app.use(express.static('client/build'));
+
+  // Set static folder & require https
+  app.use(requireHTTPS, express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
